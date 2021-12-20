25d51956edbb49db73d5ab420c743db110b6cc6b
PiotrPociask <92296161+PiotrPociask@users.noreply.github.com>
2021-12-20 11:55:25 +0100

    Update zadanie2.cpp

--- zadanie2.cpp
+++ zadanie2.cpp
@@ -4,12 +4,54 @@
 using namespace std;
 char text[100];
 
+int S(int a, int b)
+{
+	while (a != b)
+	{
+		if (a < b)
+			b -= a;
+		else
+			a -= b;
+	}
+	return a;
+}
+
 class szyfry																	
 {
 public:
 	int dlugosc;
 	int przesuniecie;
 	int r1, r2, r3;
+
+	void RSA()
+	{
+		int lpierwsze[25] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97 };
+		srand(time(NULL));
+		int x = rand() % 17;
+
+		int p = lpierwsze[x];
+		int q = lpierwsze[x + 1];
+		int r1 = p * q;
+		int r4 = (p - 1) * (q - 1);
+		int r2 = 2;
+		int r3 = 2;
+
+
+		while (r2 < r4)
+		{
+			if (S(r2, r4) == 1)
+				break;
+			else
+				r2++;
+		}
+		while (r3 * r2 % r4 != 1)
+		{
+			r3++;
+		}
+		cout << "klucz publiczny: " << "wartosc 1: " << r1 << " wartosc 2: " << r2 << endl;
+		cout << "klucz prywatny: " << "wartosc 1: " << r1 << " wartosc 2: " << r3 << endl;
+	};
+
 	void SzyfrCezar()														
 	{
 		cezar1:
@@ -71,45 +113,9 @@ public:
 		}
 	}
 
-	int S(int a, int b)
-	{
-		while (a != b)
-		{
-			if (a < b)
-				b -= a;
-			else
-				a -= b;
-		}
-		return a;
-	}
+	
 
-	void RSA()
-	{
-		int lpierwsze[25] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97 };
-			srand(time(NULL));
-				int x = rand() % 17;
-
-		int p = lpierwsze[x];
-		int q = lpierwsze[x + 1];
-		int r1 = p * q;
-		int r4 = (p - 1) * (q - 1);
-		int r2 = 2;
-		int r3 = 2;
-
-		while (r2 < r4)
-		{
-			if (S(r2, r4) == 1)
-				break;
-			else
-				r2++;
-		}
-		while (r3 * r2 % r4 != 1)
-		{
-			r3++;
-		}
-		cout << "klucz publiczny: " << "wartosc 1: " << r1 << " wartosc 2: " << r2 << endl;
-		cout << "klucz prywatny: "  << "wartosc 1: " << r1 << " wartosc 2: " << r3 << endl;
-	};
+	
 };
 
 int main()
@@ -130,14 +136,17 @@ int main()
 	int wyboropcji;
 		cin >> wyboropcji;
 	switch (wyboropcji) {
+	case 1:
 		cout << "wybrano szyfr cezara" << endl;
 		l1.SzyfrCezar();
 		break;
 	case 2:
+
 		cout << "wybrano kod przestawieniowy" << endl;
 		l1.Przestawienie();
 		break;
 	case 3:
+
 		cout << "wybrano kod przestawieniowy i szyfr cezara" << endl;
 		l1.SzyfrCezar();
 		l1.Przestawienie();
