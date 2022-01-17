58c6521ac3238ff433c63ef7faade1a4ad5510a4
PiotrPociask <92296161+PiotrPociask@users.noreply.github.com>
2022-01-17 17:31:29 +0100

    Create sort1.cpp

---
+++ sort1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+class sort {
+public:
+
+	vector<int>liczby;
+	vector<int>::iterator it;
+
+	void losowe() {
+		cout << "wprowadzanie losowych liczb do vektora" << endl;
+		srand(time(NULL));
+		for (int i = 0; i < 101; i++)
+		{
+			liczby.push_back(rand() % 1000);
+		}
+	}
+
+	void bubble() {
+		bool swapp = true;
+		while (swapp)
+		{
+			swapp = false;
+			for (int i = 0; i < liczby.size() - 1; i++)
+			{
+				if (liczby[i] > liczby[i + 1])
+				{
+					liczby[i] += liczby[i + 1];
+					liczby[i + 1] = liczby[i] - liczby[i + 1];
+					liczby[i] -= liczby[i + 1];
+					swapp = true;
+				}
+			}
+		}
+	}
+	void stawianie() {
+		int i, key, j;
+		for (i = 1; i < liczby.size() - 1; i++)
+		{
+			key = liczby[i];
+			j = i - 1;
+
+			
+			while (j >= 0 && liczby[j] > key)
+			{
+				liczby[j + 1] = liczby[j];
+				j = j - 1;
+			}
+			liczby[j + 1] = key;
+		}
+	}
+
+	void wypisywanie() {
+		for (int i = 0; i < liczby.size() - 1; i++)
+		{
+			cout << i <<". = " << liczby[i] << endl;
+		}
+	}
+	
+};
+
+int main()
+{
+	sort s1;
+
+
+
+	cout << "1 = bubble sort" << endl;
+	cout << "2 = wstawianie" << endl;
+	int wybor;
+		cin >> wybor;
+	switch (wybor)
+	{
+	case 1:
+		s1.losowe();
+		s1.wypisywanie();
+		s1.bubble();
+		s1.wypisywanie();
+		break;
+	case 2:
+		s1.losowe();
+		s1.wypisywanie();
+		s1.stawianie();
+		s1.wypisywanie();
+		break;
+	default:
+		cout << "niepoprawna opcja" << endl;
+		break;
+	}
+}
